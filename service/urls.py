7805c2150a346c69e94cbed8ceb34301671e53d7
Antonio Toro <antorof.dev@gmail.com>
2015-02-26 11:17:48 +0100

    Intermedio con templates y metodos temporales

--- service/urls.py
+++ service/urls.py
@@ -4,6 +4,7 @@ from service import views
 
 urlpatterns = patterns ('',
     url(r'^$', views.index, name='index'),
+    url(r'^index/$', views.index, name='index'),
     url(r'^compae/$', views.compae, name='compae'),
     url(r'^registro-de-(?P<fulanito>\w+)$', views.registro_, name='registro'),
     url(r'^registro-pre/(?P<suco>\w+)', views.registropro, name='registro'),
@@ -11,6 +12,7 @@ urlpatterns = patterns ('',
     url(r'^heredado/$', views.heredado, name='herencia'),
     
     url(r'^registro/$', views.registro, name='registro'),
+    url(r'^registro-avanzado/$', views.registroAvanzado, name='registro-av'),
     url(r'^login/$', views.login, name='login'),
     url(r'^bienvenida/$', views.heredado, name='herencia'),
 )
--- service/views.py
+++ service/views.py
@@ -1,10 +1,39 @@
+# -*- encoding: utf-8 -*-
+from django import forms
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponseRedirect
 from django.http import HttpResponse
+from django.core.validators import validate_slug, RegexValidator
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+import sys
+
+class NameForm(forms.Form):
+    your_name = forms.CharField(label='name', max_length=100)
+
+class loginForm(forms.Form):
+    username = forms.CharField (label = 'Nombre de usuario',max_length = 10,required = True,)
+    password = forms.CharField( label = 'Contraseña',required = True,widget = forms.PasswordInput,)
+    
+    def clean (self):
+        cleaned_data = super(loginForm, self).clean()
+
+class registroForm(forms.Form):
+    username = forms.CharField(label = 'Nombre', max_length = 10, required = True,)
+    password = forms.CharField(label = 'Contrasena', required = True, widget = forms.PasswordInput,)
+    password2 = forms.CharField(label = 'Repita su contrasena', required = True, widget = forms.PasswordInput,)
+    email = forms.EmailField(label = 'Correo electronico')
+    
+    def clean (self):
+        cleaned_data = super(registroForm, self).clean()
+        password = cleaned_data.get("pw")
+        password2 = cleaned_data.get("pw_again")
+        if password != password2:
+            raise forms.ValidationError ('las contrasenas no coinciden')
+
 
 def index (request):
-    return HttpResponse ("Hola desde servicio/")
+    return render (request, 'index.html')
     
 def compae (request):
     return HttpResponse ("La vin, compae")
@@ -25,13 +54,79 @@ def heredado (request):
     return render (request, 'heredado.html', context)
     
 def login (request):
-    context = {
-        'user':'',
-    }
-    return render (request, 'login.html', context)
+    # Si viene del POST del boton de submit
+    if request.method == 'POST':
+        form = loginForm (request.POST)
+        # Si el formulario es valido se comprueban los credenciales
+        if form.is_valid ():
+            user = authenticate(username = form.cleaned_data['username'],password = form.cleaned_data['password'])
+            if user is not None:
+                if user.is_active:
+                    context = {
+                        'username':form.cleaned_data['username'],
+                    }
+                    return render(request, 'bienvenida.html', context)
+                else:
+                    context = {
+                        'mensaje':'Usuario no activo',
+                        'form':form,
+                    }
+                    return render(request, 'login.html', context)
+            else:
+                context = {
+                    'mensaje':'Usuario o contraseña incorrectos',
+                    'form':form,
+                }
+                return render(request, 'login.html', context)
+    # Si es la primera vez que se llama (GET)
+    else:
+        fulanito = 'default'
+        form = loginForm()
+        context = {
+        'mensaje':'',
+        'form':form,
+        }
+        return render(request, 'login.html', context)
     
 def registro (request):
     context = {
         'user':'',
     }
     return render (request, 'registro.html', context)
+
+def registroAvanzado (request):
+    if request.method == 'POST':
+        form = registroForm (request.POST)
+        if form.is_valid ():
+            try:
+                User.objects.create(username = form.cleaned_data['username'],
+                    email = form.cleaned_data['email'],
+                    password = form.cleaned_data['password'])
+            except Exception as error:
+                context = {
+                    'username':'error',
+                    'form':form,
+                    'mensaje':'Ese usuario ya existe',
+                }
+                return render (request, 'registro-avanzado.html', context)
+                
+            context = {
+                'username':form.cleaned_data['username'],
+                'form':form,
+            }
+            return render (request, 'login.html', context)
+        else:
+            context = {
+                'username': 'error',
+                'form':form,
+                'mensaje':'EEERRRRRROOOOOOOOOOORR',
+            }
+            return render (request, 'registro-avanzado.html', context)
+    else:
+        username = 'default'
+        form = registroForm()
+        context = {
+            'username':username,
+            'form':form,
+        }
+        return render(request, 'registro-avanzado.html', context)
